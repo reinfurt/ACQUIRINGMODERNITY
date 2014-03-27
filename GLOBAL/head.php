b9cfec26bdd411627aa059e0ba4cf68dc82b4356
David Reinfurt <reinfurt@o-r-g.com>
2014-03-26 23:17:06 -0400

    Add animateCircle.js

--- GLOBAL/head.php
+++ GLOBAL/head.php
@@ -49,42 +49,8 @@
 	<!-- <meta charset="utf-8"> -->
 	<link rel="stylesheet" type="text/css" media="all" href="GLOBAL/global.css" />
 	<script type="text/javascript" src="GLOBAL/global.js"></script>
-
- <script type="text/javascript">
-        var canvas, context, step, steps, frames, delay;
-        var centerX, centerY, radius, direction;
-        var counter;    
- 
-        function init() {
-                canvas = document.getElementById("canvas1");
-                context = canvas.getContext("2d");
-                centerX = canvas.width / 2;
-                centerY = canvas.height / 2;
-                context.fillStyle = "white";
-                context.lineWidth = 5;
-                context.strokeStyle = "#FFF";
-                counter = 0;
-                radius = 70;
-                frames = 60;
-                step = 2.0 * Math.PI / frames;
-                delay = 25;
-                direction = 1;
-                animateNext();
-        }
- 
-        function animateNext() {
-                counter++;
-                context.clearRect(0, 0, canvas.width, canvas.height);
-                var thisStep = (counter % frames) * step * direction;
-                context.beginPath();
-                context.arc(centerX, centerY, radius, 0, thisStep, false);
-                context.stroke();
-                var t = setTimeout('animateNext()', delay);
-        }
-    </script>
-
+	<script type="text/javascript" src="GLOBAL/animateCircle.js"></script>
 	<!-- <script src="_Processing/processing-1.4.1.min.js"></script> -->
-	
 </head>
 
 <body onload="init();">
