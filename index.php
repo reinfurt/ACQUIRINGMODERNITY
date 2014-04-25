52a9a32977b042698178ec20d49ecf7ade505074
David Reinfurt <reinfurt@o-r-g.com>
2014-04-25 16:04:54 -0400

    Add movie big

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if ( ($live) || ($dev) ) {
 
 	<div id='00' class="middleContainer">
 		<video height="480" width="853" autoplay="autoplay" loop muted style="margin-top:-240px;margin-left:-426px;"> 
-			<source src="MEDIA/To-be-continued_01_white_480p_low" type="video/mp4" />
+			<source src="MEDIA/To-be-continued_01_white_480p_low.mov" type="video/mp4" />
 	  		Your browser does not support the video tag.
 		</video>
 	</div>	
